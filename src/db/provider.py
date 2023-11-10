49091d4a749aacf75827372364fd08d76c5e916d
mszypke <mszypke@bluesoft.com>
2023-11-10 22:18:56 +0100

    Removed loading env

--- src/db/provider.py
+++ src/db/provider.py
@@ -1,10 +1,8 @@
 import os
 
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-load_dotenv()
 
 engine = create_engine(os.environ.get('DATABASE_URL'))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
